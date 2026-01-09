293098f7d2bdc0eccd5e93224593693e973f4c17
Meron Hailu <hailumeron03@gmail.com>
2026-01-09 17:36:24 -0500

    sucessful working on form validation

--- Form-Creation-Validation/script.js
+++ Form-Creation-Validation/script.js
@@ -6,13 +6,47 @@ document.addEventListener('DOMContentLoaded', () => {
     const feedbackDiv = document.getElementById('form-feedback')
 
 
-    form.addEventListener("submit", (e) => {
+    form.addEventListener("submit", function (e) {
 
         e.preventDefault();
 
-        const isValid = true;
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
+
+        let isValid = true;
         const messages = [];
 
+        if (username.length < 3) {
+            isValid = false;
+            messages.push('your name should be more than 3 words');
+
+        }
+
+        if (!email.includes('@') || !email.includes('.')) {
+            isValid = false;
+            messages.push('your email is not included @');
+
+        }
+
+        if (password.length < 8) {
+            isValid = false;
+            messages.push('your password should be atleast 8 charcters');
+
+        }
+
+        feedbackDiv.style.display = 'block';
+
+        if (isValid) {
+
+            feedbackDiv.textContent = 'Registration sucessful';
+            feedbackDiv.style.color = '#28a745'
+
+        }
+        else {
+            feedbackDiv.innerHTML = messages.join('<br>');
+            feedbackDiv.style.color = "#dc3545";
+        }
 
 
     })
