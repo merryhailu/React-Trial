9b61230446c6b31b513259c8fd95ab65e3eee7e2
Meron Hailu <hailumeron03@gmail.com>
2026-01-09 13:55:43 -0500

    adding form validation task

---
+++ Form-Creation-Validation/script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback')
+
+
+    form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+
+        const isValid = true;
+        const messages = [];
+
+
+
+    })
+
+});
